2a95e13a449d5801275f5e4d7ba138c6b19514c2
MorganDawe <mdawe@discoverygarden.com>
2014-05-23 13:30:35 +0000

    Updated ingest logic.

---
+++ islandora_chemistry.api.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Hook documentation
+ */
+
+/**
+ * hook_islandora_chemistry_img_args_alter.
+ *
+ * Use this hook to modify the CLI argument used to
+ * generate TN and MEDIUM images for the chemistry
+ * solution pack on ingest.
+ *
+ * @param AbstractObject $object
+ *   The fedora object.
+ * @param string $args
+ *   The CLI arguments, as a string.
+ */
+function hook_islandora_chemistry_img_args($object, $args) {
+
+}
